c8610b061ae8c0aa2dd7a35b025ad83b05bf0a08
HarrierJFH <harrierjfh@gmail.com>
2020-05-08 11:30:49 +0800

    updated Problem237

---
+++ src/main/java/leetcode/Problem237.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class Problem237 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
