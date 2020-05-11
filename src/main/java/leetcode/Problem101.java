4f93519f8a4c2e519c9c02e545e5f95b3bad3f08
HarrierJFH <harrierjfh@gmail.com>
2020-05-11 16:07:15 +0800

    updated

---
+++ src/main/java/leetcode/Problem101.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import leetcode.structure.TreeNode;
+
+public class Problem101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null) {
+            return right == null;
+        }
+        if (right == null || left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+}
